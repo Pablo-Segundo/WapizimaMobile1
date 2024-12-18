d679bf5479c725530e4d360395ce1d6314c3a934
Pablo-DP <pablo-segundo@digital-pineapple.com.mx>
2024-12-18 16:15:15 -0600

    Initial commit
    Generated by react-native@0.76.5

---
+++ android/app/src/main/java/com/wapizimamobile/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.wapizimamobile
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "WapizimaMobile"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
